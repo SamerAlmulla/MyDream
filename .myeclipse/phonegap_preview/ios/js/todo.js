84093f65157ceef886a21a713e70e5a2d41dbe5a
SamerAlmulla <s-m-h-2011@hotmail.com>
2016-09-19 10:45:18 +0300

    Lesson 7 Broken
    LockStorage not Updatting

--- .myeclipse/phonegap_preview/ios/js/todo.js
+++ .myeclipse/phonegap_preview/ios/js/todo.js
@@ -1,20 +1,20 @@
 function getArray()
 {
 	var toDoList = new Array;                             //Create An Array
-	var taskStr = localStorage.getItem("toDoList");       //Get The Task From Input using LocalStorage
-	if(taskStr !== null)                                  //if the value is NOT empty --null--
-	     {
+	var taskStr = localStorage.getItem('toDoList');         //Get the Task from Input using LocalStorage
+	if(taskStr !== null)                                    //if the value is NOT empty --null--
+	     {                                                   //parse the data into the Array
 		     toDoList = JSON parse(taskStr);
 	     }
 		return toDoList;
 }
 
-function addtask()
+function addTask()
 {
-	var task = document.getElementById("taskName").value;        //create a variable to hold value of input
-	var array = getArray();                                                            //create a variable to hold our Array
-	Array.push(task);                                                             //pushing the task into the Array
-	localStorage.setItem(array, JSON.stringify(task));                                                        //Store the task localStorage saving to the Array
+	var task = document.getElementById("taskName").value;       //create a variable to hold value of input
+	var array = getArray();                                     //create a variable to hold our Array
+	array.push(task);                                           //pushing the task into the Array
+	localStorage.setItem('toDoList', JSON.stringify(task));     //Store the task localStorage saving to the Array
     show();
     
     return false;
@@ -25,13 +25,19 @@ function Show()
 	var array = getArray();                                 //create the variable to hold Array
 	var htmlFormat = "<ul>";                                //Add unordered List
 	
-	 for (var i=0; i < array.length; i++)                //A For Loop to display the Array 
+	 for (var i=0; i < array.length; i++)                   //A For Loop to display the Array 
 	{
 		 htmlFormat += "<li>" + array[i] + "</li>";
 	}
   htmlFormat += "</ul>";	 
-  document.getElementById(array).innerHTML = htmlFormat	                         //Show the document
+  document.getElementById('taskList').innerHTML = htmlFormat;	    //Show the document
 }
 
-document.getElementById("add").addEventListener("click",addtask);
+document.getElementById('add').addEventListener('click',addTask);
 show();
+
+
+
+
+
+
